0c40337ddab839f081ab3939dfb6890b07c9d924
iBahram <ibahram@Bahrams-MacBook-Pro.local>
2017-09-15 11:15:04 -0400

    add Store Page

---
+++ Salemize/User Interface/Pages/Store/SMStoreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SMStoreViewController.swift
+//  Salemize
+//
+//  Created by iBahram on 9/15/17.
+//  Copyright © 2017 Ebi :). All rights reserved.
+//
+
+import UIKit
+
+class SMStoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var backButton: UiButton!
+    var storeTitle: UILabel!
+    var userCoinrsIcon: UIImageView!
+    var userCoinsValue: UILabel!
+    var userTreasureIcon: UIImageView!
+    var userTreasureValue: UILabel!
+    var navigationSeparator: UIView!
+    var storeTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
